f1bb0a64c7cf37fd0abec994eb772b7d57511ebf
Ashwin Mandlik <ashwin@Ashwins-MacBook-Air.local>
2021-08-19 16:06:23 +0530

    video upload preview

--- app/src/main/java/com/atria/myapplication/VideoFragment.kt
+++ app/src/main/java/com/atria/myapplication/VideoFragment.kt
@@ -1,22 +1,47 @@
 package com.atria.myapplication
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.media.MediaMetadataRetriever
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.atria.myapplication.adapter.VideoAdapter
 import com.atria.myapplication.databinding.FragmentVideoBinding
 import com.atria.myapplication.viewModel.video.VideoFragmentViewModel
 import com.atria.myapplication.viewModel.video.VideoFragmentViewModelFactory
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 
 
 class VideoFragment : Fragment() {
 
     private lateinit var videoFragmentBinding : FragmentVideoBinding
     private lateinit var videoFragmentViewModel : VideoFragmentViewModel
+    private lateinit var but: TextView
+    private lateinit var builder: AlertDialog.Builder
+    private lateinit var fragment:VideoFragment
+
+
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,13 +54,146 @@ class VideoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         videoFragmentViewModel = ViewModelProvider(this,VideoFragmentViewModelFactory(requireContext(),requireView())).get(VideoFragmentViewModel::class.java)
         videoFragmentBinding.videosRecyclerView.layoutManager = GridLayoutManager(requireContext(),3,GridLayoutManager.VERTICAL,false)
 
         videoFragmentViewModel.getVideos {
-            videoFragmentBinding.videosRecyclerView.adapter = VideoAdapter(it,requireContext())
+            videoFragmentBinding.videosRecyclerView.adapter = VideoAdapter(it,requireContext(),this)
+        }
+        but = view.findViewById(R.id.uploadVideoButton)
+        but.setOnClickListener{
+            showdialogfun()
         }
 
     }
+
+    private fun showdialogfun() {
+        builder =
+            AlertDialog.Builder(context)
+                .setMessage("UPLOAD VIDEO")
+                .setPositiveButton("Upload from gallery") { dialog, which ->
+                    Log.i("upload from gallery", "true ")
+                    Dexter.withContext(context)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(object : PermissionListener {
+                            override fun onPermissionGranted(permissionGrantedResponse: PermissionGrantedResponse) {
+                                val intent = Intent()
+                                intent.type = "video/*"
+                                intent.action = Intent.ACTION_GET_CONTENT
+
+                               // fragment.activity?.
+                                startActivityForResult(intent, 8)
+
+                            }
+
+                            override fun onPermissionDenied(permissionDeniedResponse: PermissionDeniedResponse) {}
+                            override fun onPermissionRationaleShouldBeShown(
+                                permissionRequest: PermissionRequest,
+                                permissionToken: PermissionToken
+                            ) {
+                                permissionToken.continuePermissionRequest()
+                            }
+                        }).check()
+
+
+                }.setNeutralButton("Cancel") { dialog, which ->
+                    Log.i("cancel", "true ")
+                }.setNegativeButton("Record using camera") { dialog, which ->
+                    Log.i("upload from camera", "true ")
+                    Dexter.withContext(context)
+                        .withPermission(Manifest.permission.CAMERA)
+                        .withListener(object : PermissionListener {
+                            override fun onPermissionGranted(permissionGrantedResponse: PermissionGrantedResponse) {
+                                val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                                intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 60)
+                                //fragment.activity?.
+                                startActivityForResult(intent, 1)
+
+                            }
+
+                            override fun onPermissionDenied(permissionDeniedResponse: PermissionDeniedResponse) {}
+                            override fun onPermissionRationaleShouldBeShown(
+                                permissionRequest: PermissionRequest,
+                                permissionToken: PermissionToken
+                            ) {
+                                permissionToken.continuePermissionRequest()
+                            }
+                        }).check()
+                }
+
+        builder.show()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            val builders = AlertDialog.Builder(context)
+            builders.setCancelable(false)
+            val videoView: VideoView = VideoView(context)
+            val videoUri = data?.data
+
+            videoView.setVideoURI(videoUri)
+            videoView.start()
+
+            builders.setPositiveButton(
+                getString(R.string.upload),
+                object : DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface?, which: Int) {
+                        Toast.makeText(context, "Next", Toast.LENGTH_SHORT).show()
+                        //uploadCameraVideoToFirebase(videoUri)
+                    }
+                })
+            builders.setView(videoView).show()
+
+        }
+
+        if (resultCode == Activity.RESULT_OK && requestCode == 8) {
+            if (data?.data != null) {
+                val builders = AlertDialog.Builder(context)
+                builders.setCancelable(false)
+                val videoView: VideoView = VideoView(context)
+                var uri: Uri = data.data!!
+                videoView.setVideoURI(uri)
+                var mediaController: MediaController = MediaController(context)
+                val retriever = MediaMetadataRetriever()
+                retriever.setDataSource(context, Uri.parse(uri.toString()))
+                val time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+                var timeInMillisec = time?.toLong();
+                retriever.release()
+                if (timeInMillisec != null) {
+                    if (timeInMillisec <= 60000) {
+                        videoView.setMediaController(mediaController)
+                        videoView.start()
+                        builders.setPositiveButton(
+                            getString(R.string.upload),
+                            object : DialogInterface.OnClickListener {
+                                override fun onClick(dialog: DialogInterface?, which: Int) {
+                                    //uploadGalleryVideoToFirebase(uri)
+                                }
+                            })
+
+                        builders.setView(videoView).show()
+                        Toast.makeText(
+                            getActivity(),
+                            "Choose File successfully",
+                            Toast.LENGTH_SHORT
+                        ).show();
+                    } else {
+                        Toast.makeText(
+                            getActivity(),
+                            "Duration of video more than 60 seconds",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
 
 }
--- app/src/main/java/com/atria/myapplication/adapter/VideoAdapter.kt
+++ app/src/main/java/com/atria/myapplication/adapter/VideoAdapter.kt
@@ -1,57 +1,150 @@
 package com.atria.myapplication.adapter
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
-import android.media.MediaPlayer
+import android.media.MediaMetadataRetriever
 import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.atria.myapplication.Constants
 import com.atria.myapplication.Constants.current
 import com.atria.myapplication.Constants.extras
 import com.atria.myapplication.R
+import com.atria.myapplication.VideoFragment
 import com.atria.myapplication.WindowActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import pl.aprilapps.easyphotopicker.EasyImage
+import pl.aprilapps.easyphotopicker.MediaFile
+import java.lang.ClassCastException
+
+
 
 class VideoAdapter(
     val videos:List<String>,
-    val context:Context
+    val context:Context,
+    val fragment:VideoFragment
 ) : RecyclerView.Adapter<VideoAdapter.VideoHolder>() {
 
-    class VideoHolder(view:View) : RecyclerView.ViewHolder(view){
+    private lateinit var builder: AlertDialog.Builder
+
+    class VideoHolder(view: View) : RecyclerView.ViewHolder(view) {
         val videoView = view.findViewById<VideoView>(R.id.videoView)
         val imageView = view.findViewById<ImageView>(R.id.roundedImageView)
     }
 
+
+    private val number:String? by lazy {
+        FirebaseAuth.getInstance().currentUser?.phoneNumber
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoHolder {
-        if(viewType==11){
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.images_view,parent,false)
+        if (viewType == 11) {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.images_view, parent, false)
             return VideoHolder(view)
         }
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.videos_view,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.videos_view, parent, false)
         return VideoHolder(view)
     }
 
     override fun getItemViewType(position: Int): Int {
-        if(position==0) return 11;
+        if (position == 0) return 11;
         return super.getItemViewType(position)
     }
 
+    private fun showDialog() {
+        builder =
+            AlertDialog.Builder(context)
+                .setMessage("UPLOAD VIDEO")
+                .setPositiveButton("Upload from gallery") { dialog, which ->
+                    Log.i("upload from gallery", "true ")
+                    Dexter.withContext(context)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(object : PermissionListener {
+                            override fun onPermissionGranted(permissionGrantedResponse: PermissionGrantedResponse) {
+                                val intent = Intent()
+                                intent.type = "video/*"
+                                intent.action = Intent.ACTION_GET_CONTENT
+
+                                fragment.activity?.startActivityForResult(intent, 8)
+
+                            }
+
+                            override fun onPermissionDenied(permissionDeniedResponse: PermissionDeniedResponse) {}
+                            override fun onPermissionRationaleShouldBeShown(
+                                permissionRequest: PermissionRequest,
+                                permissionToken: PermissionToken
+                            ) {
+                                permissionToken.continuePermissionRequest()
+                            }
+                        }).check()
+
+
+                }.setNeutralButton("Cancel") { dialog, which ->
+                    Log.i("cancel", "true ")
+                }.setNegativeButton("Record using camera") { dialog, which ->
+                    Log.i("upload from camera", "true ")
+                    Dexter.withContext(context)
+                        .withPermission(Manifest.permission.CAMERA)
+                        .withListener(object : PermissionListener {
+                            override fun onPermissionGranted(permissionGrantedResponse: PermissionGrantedResponse) {
+                                val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                                intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 60)
+                                fragment.activity?.startActivityForResult(intent, 1)
+
+                            }
+
+                            override fun onPermissionDenied(permissionDeniedResponse: PermissionDeniedResponse) {}
+                            override fun onPermissionRationaleShouldBeShown(
+                                permissionRequest: PermissionRequest,
+                                permissionToken: PermissionToken
+                            ) {
+                                permissionToken.continuePermissionRequest()
+                            }
+                        }).check()
+                }
+
+    }
+
     override fun onBindViewHolder(holder: VideoHolder, position: Int) {
-        if(position==0){
+        if (position == 0) {
             holder.imageView.setImageResource(R.drawable.ic_upload)
+            holder.imageView.setOnClickListener {
+                showDialog()
+                builder.show()
+            }
             return
         }
         CoroutineScope(Dispatchers.Main).launch {
-            val link = videos[position]
+            val link = videos[position - 1]
             holder.videoView.setVideoURI(Uri.parse(link))
             val random = Random()
             holder.videoView.setOnPreparedListener {
@@ -71,8 +164,60 @@ class VideoAdapter(
 
     }
 
+
+
+
+//    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+//            val builders = AlertDialog.Builder(context)
+//            builders.setCancelable(false)
+//            val videoView: VideoView = VideoView(context)
+//            val videoUri = data?.data
+//
+//            videoView.setVideoURI(videoUri)
+//            videoView.start()
+//
+//            builders.setView(videoView).show()
+//
+//        }
+//
+//        if (resultCode == Activity.RESULT_OK && requestCode == 8) {
+//            if (data?.data != null) {
+//                val builders = AlertDialog.Builder(context)
+//                builders.setCancelable(false)
+//                val videoView: VideoView = VideoView(context)
+//                var uri: Uri = data.data!!
+//                videoView.setVideoURI(uri)
+//                var mediaController: MediaController = MediaController(context)
+//                val retriever = MediaMetadataRetriever()
+//                retriever.setDataSource(context, Uri.parse(uri.toString()))
+//                val time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+//                var timeInMillisec = time?.toLong();
+//                retriever.release()
+//                if (timeInMillisec != null) {
+//                    if (timeInMillisec <= 60000) {
+//
+//
+//                    } else {
+//                        Toast.makeText(
+//                            context,
+//                            "Duration of video more than 60 seconds",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+
     override fun getItemCount(): Int {
-        return videos.size;
-    }
+            return videos.size;
+        }
+
+
+
+
+
 
 }
